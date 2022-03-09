9013f5cdb9d0d7c16ded97cc69d28eceb3b23e93
cibo-sairai <99768829+cibo-sairai@users.noreply.github.com>
2022-03-09 14:27:55 -0800

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -2,44 +2,49 @@
 
 package Inheritance_Programs;
 
-public class Triangle extends TwoDShape
+class Triangle extends TwoDShape
 {
 
-//toString redefinition
 
-   String toString = "Triangle Class";
+//variable declaration
    double side1 = 3, side2 = 3, side3 = 3;
    double width = 3;
    double base = Math.pow(width,2);
    double side2_format = Math.pow(side2,2);
    double height = ((base)*(side2_format));
+   //constructor for Triangle Class
    public void Triangle()
    {
-   //Triangle formulas 
-   height = Math.pow((width/2),2);
-   double getArea = (height*width)/2;
-   System.out.println("Triangle Area: " + getArea);
-   System.out.println(toString);
+      //Triangle formulas 
+      //height = Math.pow((width/2),2);
+      double h = heronsHeight();
+      System.out.println("Height of Triangle: " + h);
+      System.out.println("Width of Triange: " + width);
+      Color c = Color.RED;
+      System.out.println("Color of Triangle: " + c);
+
    }
+   //heronsHeight formula
    private double heronsHeight()
    {
-   double getPerimeter = side1+side2+side3;
-   
-   return height;
+      height = Math.pow((width/2),2);
+      double getPerimeter = side1+side2+side3;   
+      return height;
+   }
+   //getArea for Triangle
+   public double getArea(double width, double height)
+   {
+         double Area = (height*width)/2;
+         return Area;
+   }
+   //toString Redefinition 
+   public String toString()
+   {
+      return "The Triangle Class implements 'heronsHeight', \n" + "Triangle Area, and Perimeter utilizing side1, side2, and side3 ints.";
    }
    
-   ///////////////////////////////////////////////////////////////////////////////////////////////
+   
 
-   public void rotate90()
-   {
-   }
-   public void rotate180()
-   {
-   }
-   public double Rotate(double degree)
-   {
-      return 20;
-   }
 
    
 }
