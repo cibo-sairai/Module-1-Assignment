cf6c9e765f22038cab97ff0f73de9aef9f4d412a
cibo-sairai <99768829+cibo-sairai@users.noreply.github.com>
2022-03-09 14:43:23 -0800

    Update TwoDShape.java

--- TwoDShape.java
+++ TwoDShape.java
@@ -8,7 +8,7 @@ import java.lang.Math.*;
 import java.util.Random;
 public class TwoDShape implements Rotate
 {
-double width, height;
+double width = 3, height = 4;
 
 //main class for module 1 assignment
    enum Color
@@ -32,19 +32,16 @@ double width, height;
    public static double getArea(int sideA, int sideB)
    {
 
-   //area
+   //area formula for rectangles
       return ((sideA^2)*(sideB^2));
    }
-   public void setHeight(double h)
-   {
-      h = 4;
-   }
+
 /*
  * Implementation of "Rotate.java" */
    @Override
    public void rotate(double degree)
    {
-      System.out.println(degree);
+      System.out.println("Rotated: " + degree);
    }
    
    @Override
@@ -62,12 +59,16 @@ double width, height;
  *  */
    public static void main(String[] args)
    {
-   //method calls, main method
+   //
+      TwoDShape z = new TwoDShape();
       
+
       System.out.println("Module 1 Program");
       System.out.println("TwoDShape Class Area Method ");
       double a_r = getArea(4,5);
       System.out.println("Rectangle Area: " + a_r);
+
+      z.rotate(90);
 
       
       Triangle t = new Triangle();
@@ -78,18 +79,23 @@ double width, height;
       System.out.println("Area of Triangle: " + a);
       String v = t.toString();
       System.out.println(v);
+      z.rotate(45);
       
+
+
       
       Circle c = new Circle(3);
-      
+
       double a_c = c.getArea(3,4);
       System.out.println("Area of Circle: " + a_c);
       String c_s = c.toString();
       System.out.println(c_s);
+      z.rotate(90);
       
 
       
    }
 }
+
 
 
